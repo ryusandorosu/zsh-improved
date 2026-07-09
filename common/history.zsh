02aca803b49fdcf39fe863779460e78033c81eb3
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-07-09 15:45:21 +0300

    moved conflicting zsh history options under separate block

--- common/history.zsh
+++ common/history.zsh
@@ -10,10 +10,12 @@ HIST_STAMPS="yyyy-mm-dd"
 # HISTORY_IGNORE="(ls|cd|pwd|exit|cd)*"
 
 setopt EXTENDED_HISTORY # записывать в историю время выполнения команды (формат: :start:elapsed;command)
-setopt INC_APPEND_HISTORY # писать команды в файл истории сразу после выполнения а не при выходе
-setopt SHARE_HISTORY # использовать общее хранилище истории во всех сеансах терминала
 setopt HIST_IGNORE_DUPS # игнорировать дубликаты команд если команда уже есть в истории
 setopt HIST_IGNORE_ALL_DUPS # удалять старую запись если новая дубликат
 setopt HIST_REDUCE_BLANKS # удалять лишние пробелы из команд перед сохранением
 setopt HIST_SAVE_NO_DUPS # не сохранять дубликаты команд
 setopt HIST_IGNORE_SPACE # не записывать команды начинающиеся с пробела
+
+# -- conflicting options -- :
+# setopt INC_APPEND_HISTORY # писать команды в файл истории сразу после выполнения а не при выходе
+setopt SHARE_HISTORY # использовать общее хранилище истории во всех сеансах терминала. INC_APPEND_HISTORY с ним конфликтует
