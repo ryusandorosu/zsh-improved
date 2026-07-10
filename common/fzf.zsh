017644a5e0cba6f52b70594d5f1f23dd7d9a7464
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-07-11 01:54:31 +0300

    finally figured out how to utilize fasd using it for fzf, made an alias for cd to use fasd

--- common/fzf.zsh
+++ common/fzf.zsh
@@ -1,10 +1,13 @@
+alias cdf='cd'
 _fzf_comprun() {
   local command=$1
   shift
   case "$command" in
-    cd)   fzf --style full --preview 'tree -C {} | head -200'      "$@" ;;
-    nvim) fzf --style full --preview 'batcat --color=always {}'    "$@" ;;
-    ssh)  fzf --style full --preview 'ping -c1 {}'                 "$@" ;;
-    *)    fzf --style full --preview 'fzf-preview.sh {}'           "$@" ;;
+    cd)   fzf --style full --preview 'tree -C {} | head -200'         "$@" ;;
+    cdf)  fasd -d | sort -nr | awk '{print $2}' \
+          | fzf --style full --preview 'tree -C {} | head -200'       "$@" ;;
+    nvim) fzf --style full --preview 'batcat --color=always {}'       "$@" ;;
+    ssh)  fzf --style full --preview 'ping -c1 {}'                    "$@" ;;
+    *)    fzf --style full --preview 'fzf-preview.sh {}'              "$@" ;;
   esac
 }
