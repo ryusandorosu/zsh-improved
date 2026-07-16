37aa2ebf1ef1e11b1efbe7e394c8e02d89e80d36
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-07-16 03:21:05 +0300

    added gitlog and gitgrep aliases using fzf, also aliases for fd and locate to use fzf too

--- common/aliases.zsh
+++ common/aliases.zsh
@@ -7,4 +7,9 @@ alias gitc='git -C'
 alias gitls='git ls-files'
 alias sudo='sudo '
 alias diffs='diff -sy --color'
+
+#fzf-using aliases:
 alias gitlog="git log --oneline | fzf --multi --style=full --preview 'git show --color {+1}'"
+alias gitgrep="git grep . | fzf --preview 'echo {1} | cut -d: -f1 | xargs -r batcat --color=always'"
+alias ffind="fd . '/' | fzf --style=full --preview='fzf-preview.sh {}'"
+alias floc="locate -b . | fzf --style=full --preview='fzf-preview.sh {}'"
