476a886d1cd31d66667e3399bc68deae3cb2c79b
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-07-18 20:49:44 +0300

    removed bat alias for debian repo binary batcat because discarded it and installed brew instead

--- common/aliases.zsh
+++ common/aliases.zsh
@@ -1,7 +1,6 @@
 alias digs='dig +short'
 alias listen='sudo ss -tulpn | grep'
 alias sed-crlf="sed -i 's/\r$//'"
-alias bat='batcat'
 alias sudo='sudo '
 alias diffs='diff -sy --color'
 alias rgrep="rgrep --color=auto --line-number --exclude-dir='.*'"
