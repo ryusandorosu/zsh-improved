b2f5302b531947f42bdf0b47a109e9cd41ae40af
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-07-22 20:23:42 +0300

    moved git-related aliases into dedicated fzf folder

--- common/aliases.zsh
+++ common/aliases.zsh
@@ -6,11 +6,4 @@ alias diffs='diff -sy --color'
 alias rgrep="rgrep --color=auto --line-number --exclude-dir='.*'"
 alias rg="rg --hidden --vimgrep --glob='!.git'"
 alias ripgrep='rg'
-
 alias gitc='git -C'
-alias gitgrep='git grep --heading --line-number --before-context=2 --after-context=1'
-
-# $ZSHREP/fzf/command-scripts/gitstatus:
-alias gitadd='gitstatus add'
-alias gitrestore='gitstatus restore'
-alias gitstaged='gitstatus restore --staged'
