6b0735b425360821106e94dda56b6703e716b06d
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-07-26 13:16:31 +0300

    moved notes to gitgrep comments

--- fzf/command-functions/gitgrep.zsh
+++ fzf/command-functions/gitgrep.zsh
@@ -5,6 +5,20 @@ source $ZSHREP/fzf/presets/main.sh
 ### to replace it
 ### alias gitgrep='git grep --heading --line-number --before-context=2 --after-context=1'
 
+# man ripgrep
+#        --context-separator=SEPARATOR
+#            The  string  used  to  separate non-contiguous context lines in the output. This is only used when one of the context flags is used (that is, -A/--after-context, -B/--before-context or -C/--context). Escape sequences like
+#            \x7F or \t may be used. The default value is --.
+
+# man git diff
+#  -D, --irreversible-delete
+#      Omit the preimage for deletes, i.e. print only the header but not the diff between the preimage and /dev/null. The resulting patch is not meant to be applied with patch or git apply; this is solely for people who want to
+#      just concentrate on reviewing the text after the change. In addition, the output obviously lacks enough information to apply such a patch in reverse, even manually, hence the name of the option.
+#      When used together with -B, omit also the preimage in the deletion part of a delete/create pair.
+#  -R
+#      Swap two inputs; that is, show differences from index or on-disk file to tree contents.
+
+
 # old gitgrep
 ggrep() {
   gitcmd=(git)
