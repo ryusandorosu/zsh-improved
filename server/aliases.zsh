74a7efca51e40c63f24fa20d7be09401655271ef
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-07-28 13:32:51 +0300

    configured gitdelta coloring settings on modified lines using ansi color names because rgb hex codes  were not correctly recognized on server

--- server/aliases.zsh
+++ server/aliases.zsh
@@ -4,6 +4,6 @@ alias etcgit='sudo git -C /etc'
 # fail2ban
 alias banlist='sudo fail2ban-client status $1'
 
-alias banlog='sudo bat /var/log/fail2ban.log'
-alias authlog='sudo bat /var/log/auth.log'
-alias nftlog='sudo bat /var/log/nftables.log'
+alias banlog='sudo cat /var/log/fail2ban.log'
+alias authlog='sudo cat /var/log/auth.log'
+alias nftlog='sudo cat /var/log/nftables.log'
