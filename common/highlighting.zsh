95d019f51f04fef0a5d45d1c26e23526546d7e05
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-08-05 03:46:06 +0300

    adjusted zsh highlighting

--- common/highlighting.zsh
+++ common/highlighting.zsh
@@ -1,9 +1,21 @@
-highlight_color="#d7af87"
-ZSH_HIGHLIGHT_HIGHLIGHTERS=( main brackets pattern )
-ZSH_HIGHLIGHT_STYLES[command]="fg=$highlight_color"
-ZSH_HIGHLIGHT_STYLES[builtin]="fg=$highlight_color"
-ZSH_HIGHLIGHT_STYLES[alias]="fg=$highlight_color"
-ZSH_HIGHLIGHT_STYLES[single-hyphen-option]='fg=cyan'
-ZSH_HIGHLIGHT_STYLES[double-hyphen-option]='fg=cyan'
-ZSH_HIGHLIGHT_STYLES[path]='fg=white'
-source ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+# https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters.md
+# https://en.wikipedia.org/wiki/ANSI_escape_code#8-bit
+
+highlight_color="#d7af87" #~222
+ZSH_HIGHLIGHT_HIGHLIGHTERS=( main brackets pattern regexp )
+
+typeset -A ZSH_HIGHLIGHT_REGEXP
+ZSH_HIGHLIGHT_REGEXP+=('\bsudo\b' fg=184,bold)
+ZSH_HIGHLIGHT_STYLES[builtin]="fg=221"
+
+ZSH_HIGHLIGHT_STYLES[command]="fg=221"
+ZSH_HIGHLIGHT_STYLES[alias]="fg=228,bold"
+
+ZSH_HIGHLIGHT_STYLES[single-hyphen-option]='fg=blue'
+ZSH_HIGHLIGHT_STYLES[double-hyphen-option]='fg=blue'
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument]='fg=172'
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument]='fg=172'
+ZSH_HIGHLIGHT_STYLES[commandseparator]='fg=241'
+ZSH_HIGHLIGHT_STYLES[path]='fg=white,bold'
+
+source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
